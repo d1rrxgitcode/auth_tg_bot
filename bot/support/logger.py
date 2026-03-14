7c0cb1e8d4e112ad9f155f3f01423cb77ebdf2e4
d1rrxgitcode <d1rrxgitcode@gmail.com>
2026-03-14 05:09:22 +0500

    feat: add docs

--- bot/support/logger.py
+++ bot/support/logger.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 
-def setup_logging(level: int = logging.DEBUG) -> None:
+def setup_logging(level: int = logging.INFO) -> None:
     fmt = logging.Formatter(
         fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
